80c9546d05186b8d9194e060cb5d0c27bed3ab3a
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-18 12:57:28 -0700

    Got Code Review

--- madlibs.py
+++ madlibs.py
@@ -19,7 +19,10 @@ AWESOMENESS = [
 def start_here():
     """Display homepage."""
 
-    return "Hi! This is the home page."
+    # return "Hi! This is the home page. "
+    return """
+    <html></html>
+    """
 
 
 @app.route('/hello')
@@ -46,17 +49,38 @@ def greet_person():
 def show_madlib_form():
     """ Ask the user if want to play game"""
     
-    play_game = request.args.get("Yes_Game")
-    no_play = request.args.get("No_Game")
+    play_game = request.args.get("game_yesno")
+    # no_play = request.args.get("No_Game")
     print play_game
 
-    if play_game:
-        return render_template("game.html", Yes_Game=play_game)
+    if play_game == "yes":
+        return render_template("game.html")
 
-    elif no_play:
+    else:
         return render_template("goodbye.html")
 
 
+@app.route('/madlib')
+def show_madlib():
+    """Takes madlib inputs and puts them into the story"""
+
+    person1 = request.args.get("person1")
+    adjective = request.args.get("Adjective")
+    verb1 = request.args.get("verb1")
+    part_body = request.args.get("Body Part")
+    number = request.args.get("Number")
+    noun = request.args.get("Noun")
+    adverb = request.args.get("Adverb")
+    verb2 = request.args.get("Verb2")
+    pronoun = request.args.get("Pronoun")
+    person2 = request.args.get("Person2")
+
+    return render_template("gameresults.html", name1=person1, description=adjective,
+        action1=verb1, bodypart=part_body, num=number, thing=noun, adv=adverb, action2=verb2, 
+        pro=pronoun, name2=person2)
+
+
+#######################################################################
 
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
